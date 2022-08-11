517dcbb333890c17e6e49a1e05595337d768e6ba
fallenreds <artur.khobotkob@nure.ua>
2022-08-11 03:49:06 +0300

    add models

--- MyCloud/cloud/admin.py
+++ MyCloud/cloud/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from .models import Folder, File, Color
 
-# Register your models here.
+admin.site.register(Folder)
+admin.site.register(File)
+admin.site.register(Color)
+
+
--- MyCloud/cloud/models.py
+++ MyCloud/cloud/models.py
@@ -1,6 +1,72 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from mptt.models import MPTTModel, TreeForeignKey
 from django.utils import timezone
+user = get_user_model()
 
-class Folder(models.Model):
-    "Папки"
-    label = models.TextField('Название',max_length=100)
+
+class Color(models.Model):
+    """Модель цветов папок"""
+    color = models.TextField('Цвет папки', max_length=100)
+
+    def __str__(self):
+        return self.color
+
+    class Meta:
+        verbose_name = 'Цвет'
+        verbose_name_plural = 'Цвета'
+
+
+class Folder(MPTTModel):
+    """Модель папок"""
+    label = models.TextField('Название', max_length=100),
+    user = models.ForeignKey(
+        user,
+        verbose_name="Пользователь",
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False
+    ),
+    parent = TreeForeignKey(
+        'self',
+        verbose_name="Родительская папка",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children'
+    )
+    color = models.ForeignKey(
+        Color,
+        verbose_name="Цвет папки",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.label
+
+    class Meta:
+        verbose_name = 'Папка'
+        verbose_name_plural = 'Папки'
+
+
+class File(models.Model):
+    """Модель файлов"""
+    label = models.TextField('Название', max_length=100),
+    filesize = models.PositiveIntegerField('Размер файла'),
+    messages = models.TextField('Ccылки на чанки в ТГ', max_length=500),
+    folder = models.ForeignKey(
+        Folder,
+        verbose_name='Папка',
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+    )
+
+    def __str__(self):
+        return self.label
+
+    class Meta:
+        verbose_name = 'Файл'
+        verbose_name_plural = 'Файлы'
